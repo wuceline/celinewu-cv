ecd2dcb227401b91e5d4a5551ada85e35f55ab25
wuceline <wuwuceline@gmail.com>
2021-12-03 12:40:51 +0100

    added title text link

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -2,7 +2,32 @@ import './styles.scss';
 
 const AboutMe = () => (
   <div className="about-me">
-    about me
+    <h1 className="title">About me</h1>
+    <p className="text">
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+    </p>
+    <p className="text">
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      Voluptate ad magna commodo exercitation cillum velit ipsum.
+      Incididunt commodo quis ad quis aliquip culpa
+      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+    </p>
+    <a href="#" className="cv-link">
+      Mon CV
+    </a>
   </div>
 );
 
