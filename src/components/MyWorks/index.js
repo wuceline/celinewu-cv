e8a0ca9bc1b9e963937bbe31c9eb21dd60557460
wuceline <wuwuceline@gmail.com>
2021-12-03 13:55:44 +0100

    edit on mywork

---
+++ src/components/MyWorks/index.js
@@ -0,0 +1,25 @@
+import './styles.scss';
+
+const MyWorks = () => (
+  <div className="my-works">
+    <h1 className="title">My work</h1>
+    <div className="work-div">
+      <a href="http://keskonmate.surge.sh/" className="work-link" target="_blank" rel="noreferrer">
+        Keskonmate
+      </a>
+      <p className="work-description">
+        Minim minim proident sint in officia occaecat nostrud est qui minim ullamco anim.
+      </p>
+    </div>
+    <div className="work-div">
+      <a href="http://keskonmate.surge.sh/" className="work-link" target="_blank" rel="noreferrer">
+        Keskonmate
+      </a>
+      <p className="work-description">
+        Minim minim proident sint in officia occaecat nostrud est qui minim ullamco anim.
+      </p>
+    </div>
+  </div>
+);
+
+export default MyWorks;
