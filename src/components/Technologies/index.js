7fc06239a69c0ea95f2d5ff1fef0df91151be06e
wuceline <wuwuceline@gmail.com>
2021-12-06 12:01:20 +0100

    added technologies

---
+++ src/components/Technologies/index.js
@@ -0,0 +1,18 @@
+import './styles.scss';
+
+const Technologies = () => (
+  <div className="technologies">
+    <h1 className="title">Technologies</h1>
+    <div className="icons">
+      <img src="https://img.icons8.com/material-rounded/90/000000/html-5.png" alt="html5" />
+      <img src="https://img.icons8.com/ios-glyphs/90/000000/css3.png" alt="css3" />
+      <img src="https://img.icons8.com/ios/90/000000/php-logo.png" alt="php" />
+      <img src="https://img.icons8.com/ios-filled/90/000000/mysql-logo.png" alt="mysql" />
+      <img src="https://img.icons8.com/ios/90/000000/javascript--v2.png" alt="javascript" />
+      <img src="https://img.icons8.com/ios-filled/90/000000/react-native.png" alt="react" />
+      <img src="https://img.icons8.com/material-outlined/90/000000/github.png" alt="github" />
+    </div>
+  </div>
+);
+
+export default Technologies;
