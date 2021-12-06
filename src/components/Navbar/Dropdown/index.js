ad22e756b80dc13f8e0ae9d3683678cff458c368
wuceline <wuwuceline@gmail.com>
2021-12-06 13:53:38 +0100

    css of dropdown

--- src/components/Navbar/Dropdown/index.js
+++ src/components/Navbar/Dropdown/index.js
@@ -3,19 +3,19 @@ import './styles.scss';
 
 const Dropdown = () => (
   <div className="dropdown">
-    <Link to="presentation" smooth duration={1000}>
+    <Link to="presentation" smooth duration={1000} className="link">
       Home
     </Link>
-    <Link to="about-me" smooth duration={1000}>
+    <Link to="about-me" smooth duration={1000} className="link">
       About me
     </Link>
-    <Link to="technologies" smooth duration={1000}>
+    <Link to="technologies" smooth duration={1000} className="link">
       Technologies
     </Link>
-    <Link to="my-works" smooth duration={1000}>
+    <Link to="my-works" smooth duration={1000} className="link">
       My works
     </Link>
-    <Link to="contact" smooth duration={1000}>
+    <Link to="contact" smooth duration={1000} className="link">
       Contact me
     </Link>
   </div>
