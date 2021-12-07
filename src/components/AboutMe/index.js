e225dc4b9cdf491888bc45d34e152c34cfd747cc
wuceline <wuwuceline@gmail.com>
2021-12-07 11:52:23 +0100

    added text

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,29 +4,43 @@ const AboutMe = () => (
   <div className="about-me">
     <h1 className="title">About me</h1>
     <p className="text">
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      I have worked in the luxury goods sales area for 6 years, at various positions, from
+      a Sales Associate to a Supervisor, to an Assistant Manager. From those few years, I have
+      learned the job itself, but also teamwork and its importance, building relationship
+      with customers, complying with the processes of a big company. But most importantly, I have
+      used that time to learn about me.
     </p>
     <p className="text">
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
-      Voluptate ad magna commodo exercitation cillum velit ipsum.
-      Incididunt commodo quis ad quis aliquip culpa
-      culpa do irure sunt cupidatat. Eu irure occaecat quis non eu reprehenderit.
+      My experience in customer service as part of a team, but also my experience in different job
+      positions offered me great assets that give me confidence and ease to build new relationships.
+      Organizing the store, training the team members and following their growth, complying with
+      the company's procedures, organizing events, creating lasting relationships with our customers
+      were all part of my work. But within that, what I liked the most was facing the challenges and
+      resolving them.
+    </p>
+    <p className="text">
+      In 2021, I seeked support from a professionnal retraining coach. It was time to challenge
+      myself, to learn new things and develop myself.
+      I started a Full stack web and web mobile developper training with
+      <a href="https://oclock.io/formations/developpeur-web#programme" className="link"> O'clock. </a>
+      I had no technical knowledge in web development but was fully convinced that I am made for it.
+      During 3 months we learned to use HTML/CSS, PHP and mySQL, JavaScript and the OOP while
+      manipulating Git with GitHub. We also had a glimpse of the power that a CMS like wordpress
+      could offer. I have then specialized myself in React for a month.
+    </p>
+    <p className="text">
+      Our project, to conclude the training, was conceived with a team of 5 during a month.
+      We worked together daily during 4 sprints.
+      You can find our presentation
+      <a href="https://www.youtube.com/watch?v=kHnBAk1Bwrg&t=896s" className="link"> here </a>
+      from 6min15 to 22min10.
+    </p>
+    <p className="text">
+      I've learned a lot during this past year as a Full stack web developper and I'm still
+      eager to lean the ocean of knowledge that lays ahead.
     </p>
     <a href="/" className="cv-link">
-      Mon CV
+      My CV
     </a>
   </div>
 );
