3ead71d441ed1f4941263ce1bc526d8a02ea70d4
wuceline <wuwuceline@gmail.com>
2021-12-10 16:28:45 +0100

    edit css on contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,11 @@
 import Form from './Form';
+import './styles.scss';
 
 const Contact = () => (
   <div className="contact">
+    <h1 className="title">Contact me</h1>
     <Form />
+    <p className="text">You can also contact me on <a href="mailto:celinewu.dev@gmail.com">celinewu.dev@gmail.com</a></p>
   </div>
 );
 
