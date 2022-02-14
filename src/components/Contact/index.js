4fda482e97fa319c81f8a97b20e7b515da030163
wuceline <wuwuceline@gmail.com>
2022-02-14 09:56:45 +0100

    edit contact info and cv

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,7 @@ const Contact = () => (
   <div className="contact">
     <h1 className="categories-title">Contact me</h1>
     <Form />
-    <p className="text">You can also contact me on <a href="mailto:celinewu.dev@gmail.com">celinewu.dev@gmail.com</a></p>
+    <p className="text">You can also contact me on <a href="mailto:wuceline.pro@gmail.com">wuceline.pro@gmail.com</a></p>
   </div>
 );
 
